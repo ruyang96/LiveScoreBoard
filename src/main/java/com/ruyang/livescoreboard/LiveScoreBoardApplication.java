41231eb2bdd0e5ef5d2dc91d746d236894b877a3
Ru Yang <ru.yang@tietoevry.com>
2024-05-03 18:22:30 +0200

    Initialize project

---
+++ src/main/java/com/ruyang/livescoreboard/LiveScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ruyang.livescoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveScoreBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveScoreBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruyang/livescoreboard/LiveScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruyang.livescoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoreBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
