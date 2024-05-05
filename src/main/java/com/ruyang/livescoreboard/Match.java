a89b5f89a521de5f453af45b7bd19a1e1994bf4d
Ru Yang <ru.yang@tietoevry.com>
2024-05-05 09:31:54 +0200

    Implement Match class

---
+++ src/main/java/com/ruyang/livescoreboard/Match.java
@@ -0,0 +1,41 @@
+package com.ruyang.livescoreboard;
+
+import lombok.Getter;
+
+@Getter
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private long homeScore;
+    private long awayScore;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
+
+    public void setHomeScore(long homeScore) {
+        if (homeScore < 0) {
+            homeScore = 0;
+        }
+        this.homeScore = homeScore;
+    }
+
+    public void setAwayScore(long awayScore) {
+        if (awayScore < 0) {
+            awayScore = 0;
+        }
+        this.awayScore = awayScore;
+    }
+
+    public Long getTotalScore() {
+        return this.homeScore + this.awayScore;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s - %s %s", homeTeam, homeScore, awayTeam, awayScore);
+    }
+}
